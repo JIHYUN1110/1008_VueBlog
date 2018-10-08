9980dd1a059762e5f14ad73fc8d97ae39a4419b0
JIHYUN1110 <jodyzzang77@likelion.org>
2018-10-08 15:40:07 +0900

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <b-card header="( ⸝⸝•ᴗ•⸝⸝ )੭⁾⁾"
+                header-text-variant="white"
+                header-tag="header"
+                header-bg-variant="info"
+                v-bind:footer="created"
+                footer-tag="footer"
+                footer-bg-variant="light"
+                footer-border-variant="dark"
+                v-bind:title="title"
+                style="max-width: 70rem; margin-left: 2rem;"
+        >
+            <p class="card-text">{{ body }}</p>
+            <div>
+                <strong>댓글</strong>
+                <comment 
+                v-for="(comment,i) in comments"
+                :key="comment.id"
+                :index="i + 1"
+                :body="comment.body"></comment>
+            </div>
+        </b-card>
+    </div>
+</template>
+
+
+
+<script>
+    import Comment from "./Comment.vue";
+    export default {
+        props: ['title','body','created','comments'],
+        components :{
+            Comment
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/Comment.vue
@@ -0,0 +1,12 @@
+<template>
+    <p> {{ index }} | {{ body }}</p>
+</template>
+
+<script>
+export default {
+    props: ["index", "body"]
+}
+</script>
+
+<style>
+</style>
